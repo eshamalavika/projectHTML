99259d04de6522b1d5bcd65010fb53d1e5a8d49a
eshamalavika <108280154+eshamalavika@users.noreply.github.com>
2023-02-24 12:56:55 +0530

    Update secondPage.js

--- secondPage.js
+++ secondPage.js
@@ -1,16 +1,25 @@
+
+
 fetch('https://reqres.in/api/users')
   .then(response => response.json())
   .then(data => {
     const users = data.data;
+    // const container=document.createElement('div');
     const list = document.createElement('ul');
+    list.classList.add("container");
     for (const user of users) {
       const listItem = document.createElement('li');
+      listItem.classList.add("userList");
       const avatar = document.createElement('img');
+      avatar.classList.add("userImage");
       avatar.src = user.avatar;
       const name = document.createElement('h3');
+      name.classList.add("userName");
       name.innerText = `${user.first_name} ${user.last_name}`;
       const email = document.createElement('p');
+      email.classList.add("userEmail");
       email.innerText = user.email;
+      // container.appendChild(list);
       listItem.appendChild(avatar);
       listItem.appendChild(name);
       listItem.appendChild(email);
